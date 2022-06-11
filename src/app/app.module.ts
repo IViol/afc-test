e1074d3fda8a092ced2f6b847e41b6defb725098
Ivan Violentov <violentoff@gmail.com>
2022-06-11 11:36:52 +0200

    Template cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegionsComponent } from './regions/regions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegionsComponent
   ],
   imports: [
     BrowserModule,
